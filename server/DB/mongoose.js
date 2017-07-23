8fea1b07dc70328b52d5cfaee6e0f47d3bafc76d
Balazs Orban <W515534@winfra.dlh.de>
2017-07-23 18:15:03 +0200

    switch to cloud db

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var connection = mongoose.createConnection('mongodb://localhost:27017/TodoApp?poolSize=4');
-//var connection = mongoose.createConnection('mongodb://test:Qwe12345@ds034797.mlab.com:34797/node-api-test');
+//var connection = mongoose.createConnection('mongodb://localhost:27017/TodoApp?poolSize=4');
+const connection = mongoose.createConnection('mongodb://test:Qwe12345@ds034797.mlab.com:34797/node-api-test');
 
 module.exports = {
     connection
